5d8d63f4a3efc0f3e4e3233a3676b47476c08ede
ratikhachv <115207713+ratikhachv@users.noreply.github.com>
2022-11-08 18:14:46 +0400

    working on sect  1 slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//collapsing searchbar
 let searchInp = document.querySelector(".search-inp")
 console.log(searchInp)
 let searchIcon = document.querySelector(".search-icon")
@@ -6,3 +8,46 @@ console.log(searchIcon);
 searchIcon.addEventListener("click", function(){
     searchInp.classList.toggle("myStyle")
 })
+
+// slider
+let previous = document.querySelector(".previous")
+let next = document.querySelector(".next")
+let imgDiv = document.querySelector(".slider")
+let slidesArray = document.getElementsByClassName("slider-card")
+console.log(slidesArray.length);
+console.log(imgDiv);
+
+let clickCounter = 0
+
+previous.addEventListener("click", function(){
+    if(clickCounter == 0){
+        clickCounter =slidesArray.length - 1
+        let transitionAmount = clickCounter * -100
+        imgDiv.style.transform =`translateX(${transitionAmount}%)`
+        imgDiv.style.transitionDuration = "0s"
+    }else{
+        clickCounter--
+        let transitionAmount = clickCounter * -100
+        console.log(`translateX(${transitionAmount}%)`)
+        imgDiv.style.transform =`translateX(${transitionAmount}%)`
+        imgDiv.style.transitionDuration = "2s"
+    }
+})
+
+next.addEventListener("click", function(){
+    if(clickCounter >= slidesArray.length - 1){
+        clickCounter = 0
+        let transitionAmount = 0
+        imgDiv.style.transform =`translateX(${transitionAmount}%)`
+        imgDiv.style.transitionDuration = "0s"
+    }else{
+        clickCounter++
+        let transitionAmount = clickCounter * -100
+        console.log(`translateX(${transitionAmount}%)`)
+        imgDiv.style.transform =`translateX(${transitionAmount}%)`
+        imgDiv.style.transitionDuration = "2s"
+    }
+})
+
+
+console.log(clickCounter);
